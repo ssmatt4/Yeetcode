ecdf1e789f4ef6d842945ec185abc99446864d63
ssmatt4 <ssmatthew2004@gmail.com>
2025-01-24 19:53:43 -0500

    forgot to add pages

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,10 @@
+import Button from "../components/Button/Button.tsx";
+
+export default function Home() {
+    return (
+        <>
+            <h1>Welcome to Yeetcode!</h1>
+            <Button title="Create New Game"/>
+        </>
+    )
+}
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,10 @@
+import Button from "../components/Button/Button.tsx";
+
+export default function Login() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <Button title="Login"/>
+        </div>
+    );
+}
